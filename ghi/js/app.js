2ae54febf305002f20dac62167ea46829e530634
Henry Kim <hnrykm@gmail.com>
2023-05-23 12:33:27 -0400

    Data to DOM completed

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -6,6 +6,18 @@ window.addEventListener("DOMContentLoaded", async () => {
 			console.log("There was a problem with your request.");
 		} else {
 			const data = await response.json();
+			const conference = data.conferences[0];
+			const nameTag = document.querySelector(".card-title");
+			nameTag.innerHTML = conference.name;
+
+			const detailUrl = `http://localhost:8000${conference.href}`;
+			const detailResponse = await fetch(detailUrl);
+			if (detailResponse.ok) {
+				const details = await detailResponse.json();
+				const description = details.conference.description;
+				const detailTag = document.querySelector(".card-text");
+				detailTag.innerHTML = description;
+			}
 		}
 	} catch (e) {
 		console.log("There was a problem with your request.");
