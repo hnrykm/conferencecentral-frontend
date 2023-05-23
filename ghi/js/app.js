1d766f5a8bdfb39c18693bdb0ebd1cae0ba2b880
Henry Kim <hnrykm@gmail.com>
2023-05-23 12:14:00 -0400

    The First Fetch completed

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,13 @@
-window.addEventListener('DOMContentLoaded', () => {
-
-
+window.addEventListener("DOMContentLoaded", async () => {
+	const url = "http://localhost:8000/api/conferences/";
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			console.log("There was a problem with your request.");
+		} else {
+			const data = await response.json();
+		}
+	} catch (e) {
+		console.log("There was a problem with your request.");
+	}
 });
