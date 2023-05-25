829a9f87c356b3f8878c517cc871ff729301f46f
Henry Kim <hnrykm@gmail.com>
2023-05-25 07:01:56 -0400

    Stretch goal completed

--- ghi/js/new-presentation.js
+++ ghi/js/new-presentation.js
@@ -1,31 +1,34 @@
 function createSuccess() {
-	return `<div class="alert alert-success" role="alert">New location successfully created!</div>`;
+	return `<div class="alert alert-success" role="alert">New presentation successfully created!</div>`;
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
-	const url = "http://localhost:8000/api/states/";
+	const url = "http://localhost:8000/api/conferences/";
 	const response = await fetch(url);
 
 	if (response.ok) {
 		const data = await response.json();
 
-		const selectTag = document.getElementById("state");
-		for (let state of data.states) {
+		const selectTag = document.getElementById("conference");
+		for (let conference of data.conferences) {
 			const option = document.createElement("option");
-			option.value = state.abbreviaton;
-			option.innerHTML = state.name;
+			option.value = conference.id;
+			option.innerHTML = conference.name;
 			selectTag.appendChild(option);
 		}
 	}
 
-	const formTag = document.getElementById("create-location-form");
+	const formTag = document.getElementById("create-presentation-form");
 	formTag.addEventListener("submit", async (event) => {
 		event.preventDefault();
+
+		const select = document.getElementById("conference");
+		const conference_id = select.options[select.selectedIndex].value;
 
 		const formData = new FormData(formTag);
 		const json = JSON.stringify(Object.fromEntries(formData));
 
-		const locationUrl = "http://localhost:8000/api/locations/";
+		const presentationUrl = `http://localhost:8000/api/conferences/${conference_id}/presentations/`;
 		const fetchConfig = {
 			method: "POST",
 			body: json,
@@ -34,12 +37,12 @@ window.addEventListener("DOMContentLoaded", async () => {
 			},
 		};
 
-		const response = await fetch(locationUrl, fetchConfig);
+		const response = await fetch(presentationUrl, fetchConfig);
 		if (response.ok) {
 			formTag.reset();
 			const success = document.getElementById("submitted");
 			success.innerHTML = createSuccess();
-			const newLocation = await response.json();
+			const newPresentation = await response.json();
 		}
 	});
 });
